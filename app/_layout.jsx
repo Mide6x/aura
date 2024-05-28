fbe024589020a1bfbba5868208170e624c7efa6c
Olumide <adewoleolumide05@gmail.com>
2024-05-28 11:15:58 +0100

    added fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
 
 const RootLayout = () => {
+  const [fontLoaded, error] = useFonts({
+    "Poppins-Black": require("../fonts/Poppins-Black.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (useFonts) SplashScreen.hideAsync();
+  }, [error]);
+
+  if (!error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
