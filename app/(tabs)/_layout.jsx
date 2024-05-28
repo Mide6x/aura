d7eab6fb6b85a5dd3c9e51a6d162fc206a05e926
Olumide <adewoleolumide05@gmail.com>
2024-05-28 13:15:13 +0100

    updated files

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import React from "react";
 import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
+import Home from "./home";
 
-const TabImage = () => {
-  return <Text></Text>;
+const TabIcon = ({ name, icon, color, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+    </View>
+  );
 };
 
 const TabLayout = () => {
@@ -14,24 +25,56 @@ const TabLayout = () => {
           name="home"
           options={{
             title: "Home",
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
           }}
         />
         <Tabs.Screen
           name="create"
           options={{
             title: "Create",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.upload}
+                color={color}
+                name="Upload"
+                focused={focused}
+              />
+            ),
           }}
         />
         <Tabs.Screen
           name="bookmarks"
           options={{
             title: "Bookmarks",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="bookmark"
+                focused={focused}
+              />
+            ),
           }}
         />
         <Tabs.Screen
           name="profile"
           options={{
             title: "Profile",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="profile"
+                focused={focused}
+              />
+            ),
           }}
         />
       </Tabs>
