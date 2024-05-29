432c3a84b9b418604ccae22201380cdc4561674d
Olumide <adewoleolumide05@gmail.com>
2024-05-29 17:03:39 +0100

    updated files

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, ScrollView, Image } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { React } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../constants";
+import ClickinButton from "../components/ClickinButton";
 
 export default function index() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full flex justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -18,17 +19,31 @@ export default function index() {
           />
           <Image
             source={images.cards}
-            className="max-w-[300px] w-full h-[304px]"
+            className="max-w-[380px] w-full h-[298px]"
             resizeMode="contain"
           />
-        </View>
-        <View className="relative mt-5">
-          <Text className="font-bold text-3xl text-white text-center">
-            Discover Endless Possibilities With{" "}
-            <Text className="text-secondary-200">Aura</Text>
+          <View className="relative mt-5">
+            <Text className="font-bold text-3xl text-white text-center">
+              Discover Endless {"\n"} Possibilities With{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Get on an amazing Journey of
+            Exploration with Aora
           </Text>
+          <ClickinButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 // tailwind.config.js
 
 module.exports = {
-  content: ["./index.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
